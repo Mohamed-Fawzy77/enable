8093634674abe3aca70c5bce196bd5ac83e1703f
fawzy <fawzy@garment.io>
2021-11-11 08:56:47 +0000

    add role model

---
+++ app/modules/role/role.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const rolesNames = ['Super Admin Role', 'Department Manager', 'Employee'];
+
+const RoleSchema = new mongoose.Schema({
+    name: { type: String, enum: rolesNames, required: true },
+    permissions: [{ type: mongoose.Schema.Types.ObjectId }]
+},
+    { timestamps: true }
+);
+
+const Role = mongoose.model('User', RoleSchema);
+
+module.exports = Role;
--- app/modules/user/user.model.js
+++ app/modules/user/user.model.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String },
-    department: { type: Schema.Types.ObjectId, ref: 'Department' },
-    role: { type: Schema.Types.ObjectId, ref: 'Role' },
+    department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department' },
+    role: { type: mongoose.Schema.Types.ObjectId, ref: 'Role' },
 },
     { timestamps: true }
 );
 
 const User = mongoose.model('User', UserSchema);
+
+module.exports  = User;
